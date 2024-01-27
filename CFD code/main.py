ad72b1e6ea4c01b3d3f844fc5f6c20965de4ed3d
KaiyuLi-1891 <138957847+KaiyuLi-1891@users.noreply.github.com>
2024-01-27 13:41:34 +0800

    Add files via upload
    Open this file and try to do some modifications from your side.

---
+++ CFD code/main.py
@@ -0,0 +1,12 @@
+
+# Let's have a try to add our names in the main function!
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('励凯宇')
+
+
